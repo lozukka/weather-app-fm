10bf61667f66ce45b025a3ad82d228c5b3156801
lozukka <lotsulotsu@gmail.com>
2025-09-09 18:49:39 +0300

    change city to cordinates

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log("hello");
 let openDropdown = document.getElementById("open-units-dropdown");
 let dropdown = document.getElementById("dropdown");
 let searchBtn = document.getElementById("search-btn");
@@ -14,7 +13,7 @@ openDropdown.addEventListener("click", (event) => {
 searchBtn.addEventListener("click", (event) => {
   event.preventDefault();
   let city = document.getElementById("city").value;
-
+  getCordinates(city);
   let resultCityDisplay = document.getElementById("search-result-city");
   resultCityDisplay.textContent = city;
 
@@ -30,3 +29,23 @@ searchBtn.addEventListener("click", (event) => {
   let resultDateDisplay = document.getElementById("search-result-date");
   resultDateDisplay.textContent = formatted;
 });
+
+function getCordinates(city) {
+  //const city = "Helsinki";
+  const geoUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(
+    city
+  )}&count=1&language=fi&format=json`;
+
+  fetch(geoUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      if (!data.results || data.results.length === 0) {
+        throw new Error("Kaupunkia ei löytynyt!");
+      }
+      const { latitude, longitude, name, country } = data.results[0];
+      console.log(`Kaupunki: ${name}, ${country} (${latitude}, ${longitude})`);
+
+      // Tässä kohtaa voit käyttää koordinaatteja säähaussa
+    })
+    .catch((err) => console.error(err));
+}

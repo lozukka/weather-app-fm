3ebf2871a66c4f6b5da5552a3a192bb27bbbb3c7
lozukka <lotsulotsu@gmail.com>
2025-09-09 15:12:32 +0300

    dropdow

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("hello")
+let openDropdown = document.getElementById("open-units-dropdown");
+let dropdown = document.getElementById("dropdown");
+
+openDropdown.addEventListener("click", (event)=>{
+    if(dropdown.style.display==="block"){
+        dropdown.style.display="none"
+    }else{
+    dropdown.style.display="block";}
+})

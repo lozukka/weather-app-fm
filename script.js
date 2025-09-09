82ea16155af9ec67c80f32c7e4254ef0f3988449
lozukka <lotsulotsu@gmail.com>
2025-09-09 18:42:50 +0300

    how the date is shown

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-console.log("hello")
+console.log("hello");
 let openDropdown = document.getElementById("open-units-dropdown");
 let dropdown = document.getElementById("dropdown");
+let searchBtn = document.getElementById("search-btn");
 
-openDropdown.addEventListener("click", (event)=>{
-    if(dropdown.style.display==="block"){
-        dropdown.style.display="none"
-    }else{
-    dropdown.style.display="block";}
-})
+openDropdown.addEventListener("click", (event) => {
+  if (dropdown.style.display === "block") {
+    dropdown.style.display = "none";
+  } else {
+    dropdown.style.display = "block";
+  }
+});
+
+searchBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  let city = document.getElementById("city").value;
+
+  let resultCityDisplay = document.getElementById("search-result-city");
+  resultCityDisplay.textContent = city;
+
+  let searchDate = new Date();
+  let options = {
+    weekday: "long",
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  };
+  let formatted = searchDate.toLocaleDateString("en-US", options);
+  console.log(formatted);
+  let resultDateDisplay = document.getElementById("search-result-date");
+  resultDateDisplay.textContent = formatted;
+});
